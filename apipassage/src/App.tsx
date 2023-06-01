65896214c59ee138e9cc8040ad7f75cbefecd2bc
Tyler Diderich <tylerdiderich@gmail.com>
2023-06-01 12:08:07 -0500

    initial commit

---
+++ apipassage/src/App.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import logo from "./assets/transparent-logo.svg";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} alt="logo" />
+        <h1>API Passage</h1>
+        <p>One API For All Of Your Security Tools</p>
+      </header>
+      <header className="App-header">
+        <img src={logo} alt="logo" />
+        <h1>API Passage</h1>
+        <p>One API for all of your security tools</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ apipassage/src/secrets/sample-firebase.tsx
@@ -0,0 +1,10 @@
+// 
+export const firebaseConfig = {
+  apiKey: "UPDATE_ME",
+  authDomain: "UPDATE_ME",
+  projectId: "UPDATE_ME",
+  storageBucket: "UPDATE_ME",
+  messagingSenderId: "UPDATE_ME",
+  appId: "UPDATE_ME",
+  measurementId: "UPDATE_ME",
+};
