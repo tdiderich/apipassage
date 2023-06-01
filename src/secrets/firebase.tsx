7f38dbc3b1707acd29cc7e4df0749db727e2ecba
Tyler Diderich <tylerdiderich@gmail.com>
2023-06-01 12:17:28 -0500

    update structure

---
+++ src/secrets/firebase.tsx
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDpxkdQYWsxHK6Oit8RBZTUe-NAiLTH57c",
+  authDomain: "apipassage.firebaseapp.com",
+  projectId: "apipassage",
+  storageBucket: "apipassage.appspot.com",
+  messagingSenderId: "256217368185",
+  appId: "1:256217368185:web:96739cf21177f9093736a8",
+  measurementId: "G-57F3N399EV",
+};
